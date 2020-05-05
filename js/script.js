c498856db238c6199f998eb6853477a2710c3a58
climo_studios <climostudios@gmail.com>
2020-05-06 08:19:34 +1200

    Setting the document up

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$("#navListButton").click(function(){
+    $("#navList").slideToggle();
+});
+
+
+let winH = $(window).height() * 0.8;
+
+$(window).on('scroll', function () {
+    if ($(this).scrollTop() > winH ) {
+        $('#nav').addClass('bgRed');
+        $('#headerImg').attr('src', 'images/colab-logo-alt.png');
+    } else {
+        $('#nav').removeClass('bgRed');
+        $('#headerImg').attr('src', 'images/colab-logo-white.png');
+    }
+}).on('resize', function(){
+   winH = $(this).height() * 0.8;
+});
