5a1894f589deaba010e720d8cbd42f766e1b53dd
climo_studios <climostudios@gmail.com>
2020-05-06 14:46:35 +1200

    Final push added instagram

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ let winH = $(window).height() * 0.8;
 $(window).on('scroll', function () {
     if ($(this).scrollTop() > winH ) {
         $('#nav').addClass('bgRed');
-        $('#headerImg').attr('src', 'images/colab-logo-alt.png');
+        $('#headerImg').attr('src', 'media/logo2.png');
     } else {
         $('#nav').removeClass('bgRed');
-        $('#headerImg').attr('src', 'images/colab-logo-white.png');
+        $('#headerImg').attr('src', 'media/logo2.png');
     }
 }).on('resize', function(){
    winH = $(this).height() * 0.8;
