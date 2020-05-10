cd756bed49e3557370ace43d51e48973f5877d6c
clasomato <jfclimo@gmail.com>
2020-05-10 19:37:49 +1200

    Fixed up the home page and made it look nice and optimized

--- js/script.js
+++ js/script.js
@@ -16,3 +16,21 @@ $(window).on('scroll', function () {
 }).on('resize', function(){
    winH = $(this).height() * 0.8;
 });
+
+
+
+// Slideshow JS
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
